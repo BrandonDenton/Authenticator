e5d290378ef4c00e13d5038b1e13b1b4e55df24e
BrandonDenton <oinkerbob1@gmail.com>
2016-11-21 17:52:04 -0500

    Flask fixes

--- server/server.py
+++ server/server.py
@@ -1,4 +1,4 @@
-#!usr/bin/env python
+#!usr/bin/env python3
 ########################################################################
 # JSON-Receiving and Parsing TCP Server
 # AUTHOR: Brandon Denton
@@ -12,80 +12,85 @@
 import socket as S
 import os    # We need to make a directory to store user keys and files to be sent.
 from time import gmtime, strftime   # timestamping phone data files
-#import pymysql.cursors
+import MySQLdb
+from flask import Flask, request
+import json
 
+## My scheme for the server requires it to listen for requests    ##
+## on three ports, one for each required service, listed below:   ##
+##                                                                ##
+##    EDIT: Just listen for training data on port 80.             ##
+##    port 24601 - receive data file from client(At last,         ##
+##                 Valjean, we see each other plain!)             ##
+##    port 42069 - send training file (pump out dank memes)       ##
 
-## My scheme for the server requires it to listen for requests on three ports, ##
-## one for each required service, listed below:                                ##
-##                                                                             ##
-##    port 24601 - receive training file (At last, Valjean, we see each other plain!) ##
-##    port 24602 - message update requests                                     ##
-##    port 42069 - send training file (pump out dank memes)               ##
-
-#def SQLinsert(jsonthing):
-#   conn = pymysql.connect(host = "us", user = "", password = "", db = "")
-#
-#   try:   
-#       with conn.cursor() as cur:
-#           cur.execute("INSERT INTO db training VALUES (%s)", jsonthing)
-#       conn.commit()
-#   finally:
-#       conn.close()
-
-def servRun(servlogs):
-    sendsock = S.socket(S.AF_INET, S.SOCK_STREAM)    # may use this for a service idk
-    updatesock = S.socket(S.AF_INET, S.SOCK_STREAM)    # listen for message update requests
-    grabsock = S.socket(S.AF_INET, S.SOCK_STREAM)    # listen for new files to send
-
-    host = S.gethostname()           # grab this machine's name
-    servlogs.write(host)
-    grabsock.bind((host, 24601))     # bind host to socket to receive files from clients
-
-    grabsock.listen(5)
-    servlogs.write("##### START LISTENING #####")
-    sendconn = -2    # flags for while loop below
-    
-    while True:    # continuously listen on all three ports
-        ## Receive a file a user wants to send. ##
-        timestamp = strftime("%Y-%m-%d_%H-%M-%S", gmtime())
-        jsonfname = timestamp + ".json"
-        fin = open(jsonfname, "wb")    # file we will create
-
-        while sendconn == -2:
-            sendconn, clientaddr = grabsock.accept()    # Now prepare to write the contents of an encrypted file!
-    
-        
-            servlogs.write(strftime("%Y-%m-%d_%H-%M-%S", gmtime()) + "    grabbing usage data from " + clientaddr)
-            flag, i = "1", 0
-            flag = sendconn.recv()   # If this flag is ever set to zero, the client is done sending files. 
-            
-            ## Now record the encrypted file the client user has sent. ##
-            while(flag == "1"):
-                fileContents = sencconn.recv()
-                name = str(i) + "_"
-                f = open(name, "wb")
-                f.write(fileContents)
-            
-                ## SQL Insertion ##
-                # SQLinsert(filecontents)
-            
-                f.close()
-            
-                i += 1
-                flag = sendconn.recv()    # will send "1" if more files, "0" if no more
-            
-            sendconn.close()
-            sendconn = -2
-        grabsock.listen(5)
-        servlogs.write(strftime("%Y-%m-%d_%H-%M-%S", gmtime()) + "    Listening for recieve requests....")
-        
+app = Flask(__name__)
+@app.route("/", methods=["GET", "POST"])
 def start():
     logstamp = strftime("%Y-%m-%d_%H-%M-%S", gmtime())
     logfile = logstamp + ".txt"
     servlogs = open(logfile, "wb")
     
-    servRun(servlogs)
-    servlogs.close()    # kill log reading if something goes wrong
+    gibeJSON()
+    #servRun(servlogs)
+    servlogs.close()    # kill log writing if something goes wrong
+
+def gibeJSON():
+    print(request)
+    flat = flatten_json(request.json)
+    SQLinsert(flat);    # MySQL
+    
+    # Write the file for training.
+    timestamp = strftime("%Y-%m-%d_%H-%M-%S", gmtime())
+    fname = timestamp + ".txt"
+    with open(fname, "wb") as f:
+        f.write(flat)
+    os.system("mv fname ~/Desktop/Authenticator/training-data")    # We don't want MALLET to train on our logfiles. :0
+
+    # Format the text file properly and build topic models.
+    os.system("bin/mallet import-dir --input ~/Desktop/Authenticator/training-data --output topic-input.mallet \
+  --keep-sequence --remove-stopwords")
+    os.system("bin/mallet train-topics --input topic-input.mallet \
+  --num-topics 100 --output-state topic-state.gz")
+    
+    # cleanup
+    os.system("rm topic-input.mallet")
+
+    return "success"
+
+def flatten_json(y):
+    out = {}
+
+    def flatten(x, name=''):
+        if type(x) is dict:
+            for a in x:
+                flatten(x[a], name + a + '_')
+        elif type(x) is list:
+            i = 0
+            for a in x:
+                flatten(a, name + str(i) + '_')
+                i += 1
+        else:
+            out[name[:-1]] = x
+
+    flatten(y)
+    return out
+
+def SQLinsert(jsonthing):
+    print("Connecting to database....")
+    with open("dbcreds.conf") as conff:
+        conflist = conff.read().splitlines()
+
+    db = MySQLdb.connect(host = conflist[0], user = conflist[2], passwd = conflist[3], db = conflist[1])
+
+    try:   
+        with conn.cursor() as cur:
+            cur.execute("INSERT INTO db training VALUES (%s)", jsonthing)
+        conn.commit()
+    finally:
+        conn.close()
+
 
 if __name__ == '__main__':
-    start()
+    app.debug = True
+    app.run(threaded=True, host='0.0.0.0', port=42069)
